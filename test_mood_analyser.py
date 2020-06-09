d29e39b3ece44ef3df224615a96116dc09b62571
pranjalikawale <pranjalikawale1578@gmail.com>
2020-06-09 09:39:39 +0530

    ADDED:Create Factory for mood analyser object

--- test_mood_analyser.py
+++ test_mood_analyser.py
@@ -38,3 +38,23 @@ class TestMoodAnalyser:
             analyser_object.analyse_mood()
         assert str(exception.value)=="None mood is invalid"
 
+        def test_pass_when_given_mood_analyse_class_should_return_object(self,mood_analyser_object):
+        assert mood_analyser_object.equals(MoodAnalyserFactory.create_mood_analyser_object
+                                          ("mood_analyser","MoodAnalyser"))==True
+
+    def test_pass_when_given_mood_analyse_class_with_message_should_return_object(self, mood_analyser_object_with_happy):
+        assert mood_analyser_object_with_happy.equals(MoodAnalyserFactory.create_mood_analyser_object
+                                          ("mood_analyser","MoodAnalyser","I am happy"))==True
+
+    def test_pass_when_given_wrong_class_should_throw_module_not_found_exception(self,mood_analyser_object):
+        with pytest.raises(MoodAnalyserException) as exception:
+            mood_analyser_object.equals(MoodAnalyserFactory.create_mood_analyser_object
+                                          ("mood_analyser","MoodAnalyser1"))
+        assert str(exception.value)=="Module not found error"
+        
+    def test_pass_when_given_filepath_should_throw_module_not_found_exception(self, mood_analyser_object):
+        with pytest.raises(MoodAnalyserException) as exception:
+            mood_analyser_object.equals(MoodAnalyserFactory.create_mood_analyser_object
+                                          ("mood_analyser1","MoodAnalyser"))
+        assert str(exception.value)=="Module not found error"
+
