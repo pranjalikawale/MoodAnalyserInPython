6db63ef583b262790d26b5271c199e9debc64ed9
pranjalikawale <pranjalikawale1578@gmail.com>
2020-06-09 09:33:01 +0530

    ADDED: Analyse mood using parameterize constructor

--- test_mood_analyser.py
+++ test_mood_analyser.py
@@ -20,3 +20,21 @@ class TestMoodAnalyser:
         with pytest.raises(MoodAnalyserException) as exception:
             mood_analyser_object.analyse_mood()
         assert str(exception.value)=="Empty mood is invalid"
+    
+    def test_pass_when_contain_sad_by_constructor(self,mood_analyser_object_with_sad):
+        assert mood_analyser_object_with_sad.analyse_mood()=="sad"
+
+    def test_passed_when_contain_happy_by_constructor(self,mood_analyser_object_with_happy):
+        assert mood_analyser_object_with_happy.analyse_mood()=="happy"
+
+    def test_passed_given_empty_message_to_constuctor_then_throws_exception(self,mood_analyser_object):
+        with pytest.raises(MoodAnalyserException) as exception:
+            mood_analyser_object.analyse_mood()
+        assert str(exception.value)=="Empty mood is invalid"
+        
+    def test_passed_given_none_message_to_constuctor_then_throws_exception(self):
+        with pytest.raises(MoodAnalyserException) as exception:
+            analyser_object=MoodAnalyser(None)
+            analyser_object.analyse_mood()
+        assert str(exception.value)=="None mood is invalid"
+
