4afedab5ade586c4991ec3430c76f80f1bb23786
pranjalikawale <pranjalikawale1578@gmail.com>
2020-06-09 09:24:40 +0530

    ADDED: Handle empty mood exception

--- test_mood_analyser.py
+++ test_mood_analyser.py
@@ -5,13 +5,18 @@ from  mood_analyser_factory import MoodAnalyserFactory
 
 class TestMoodAnalyser:
 
-    def test_passed_when_contain_sad(self,mood_analyser_object):
+    def test_pass_when_contain_sad(self,mood_analyser_object):
         assert mood_analyser_object.analyse_mood("I am Sad")=="sad"
 
-    def test_passed_when_contain_happy(self,mood_analyser_object):
+    def test_pass_when_contain_happy(self,mood_analyser_object):
         assert mood_analyser_object.analyse_mood("I am Happy")=="happy"
 
-    def test_passed_if_none_message_then_throws_exception(self,mood_analyser_object):
+    def test_pass_when_none_message_then_throws_exception(self,mood_analyser_object):
         with pytest.raises(MoodAnalyserException) as exception:
             mood_analyser_object.analyse_mood(None)
         assert str(exception.value)=="None mood is invalid"
+
+    def test_pass_when_empty_message_then_throws_exception(self,mood_analyser_object):
+        with pytest.raises(MoodAnalyserException) as exception:
+            mood_analyser_object.analyse_mood()
+        assert str(exception.value)=="Empty mood is invalid"
