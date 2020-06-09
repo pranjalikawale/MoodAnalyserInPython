2f679d6a3867b2b46d4d5d813a3dfa41beea7a1c
pranjalikawale <pranjalikawale1578@gmail.com>
2020-06-09 09:19:05 +0530

    ADDED: Handle null point exception

--- test_mood_analyser.py
+++ test_mood_analyser.py
@@ -10,3 +10,8 @@ class TestMoodAnalyser:
 
     def test_passed_when_contain_happy(self,mood_analyser_object):
         assert mood_analyser_object.analyse_mood("I am Happy")=="happy"
+
+    def test_passed_if_none_message_then_throws_exception(self,mood_analyser_object):
+        with pytest.raises(MoodAnalyserException) as exception:
+            mood_analyser_object.analyse_mood(None)
+        assert str(exception.value)=="None mood is invalid"
